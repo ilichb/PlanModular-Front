1cc6cecda4a0fca828ccb5f40fb1b2a0141f4814
Javier Ochoa <jochoaalv@gmail.com>
2023-04-27 21:59:09 -0500

    full dashboard mockup

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
-<?php
+ <?php
 //    Variable en PHP
     $branding = 25;
+    $organic_growth = 18;
+    $total_growth = 50;
+    $seo_level = 42;
+    $sales = 80;
+    $projected_earnings = 120000;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,25 +15,112 @@
     <title>Title</title>
 </head>
 <body>
-    <div class="chartContainer">
-        <div class="brand-chart-container">
-            <div class="brand-chart-parent">
-                <canvas id="brand-chart"></canvas>
+    <div class="dashboard">
+        <div class="left-container">
+        <div class="left-up-left-container">
+            <div class="brand-chart-container">
+                <div class="brand-chart-parent">
+                    <canvas id="brand-chart"></canvas>
+                </div>
+                <div class="brand-chart-text-container">
+                    <p>Reconocimiento de marca</p>
+                    <!-- Aqui se lee de PHP para el porcentaje -->
+                    <p class="percentage-number"><?= $branding ?>%</p>
+                </div>
             </div>
-            <div class="brand-chart-text-container">
-                <p>Reconocimiento de marca</p>
-                <!-- Aqui se lee de PHP para el porcentaje -->
-                <p class="branding-percentage"><?= $branding ?>%</p>
+            <div class="organic-growth-container">
+                <div class="organic-growth-doughnut-container">
+                    <div class="organic-growth-doughnut-parent">
+                        <canvas id="organic-growth-doughnut-chart"></canvas>
+                    </div>
+                    <div class="organic-growth-doughnut-text-container">
+                        <p>Crecimiento organico</p>
+                        <p class="percentage-number"><?= $organic_growth ?>%</p>
+                    </div>
+                </div>
+                <div class="organic-growth-linear-parent">
+                    <canvas id="organic-growth-linear-chart"></canvas>
+                </div>
             </div>
         </div>
-        <div>
-            <canvas id="secondChart"></canvas>
+        <!--     Map PLACEHOLDER       -->
+        <div class="world-map-container">
+            <canvas id="world-map-chart"></canvas>
         </div>
+        <div class="left-down-container">
+            <div class="total-growth-container">
+                <div class="total-growth-doughnut-container">
+                    <div class="total-growth-doughnut-parent">
+                        <canvas id="total-growth-doughnut-chart"></canvas>
+                    </div>
+                    <div class="total-growth-text-container">
+                        <p>Crecimiento Total en Redes Sociales</p>
+                        <p class="percentage-number"><?= $total_growth ?>%</p>
+                    </div>
+                </div>
+                <div class="total-growth-linear-parent">
+                    <canvas id="total-growth-linear-chart"></canvas>
+                </div>
+            </div>
+            <div class="seo-container">
+                <div class="seo-parent">
+                    <canvas id="seo-chart"></canvas>
+                </div>
+                <div class="seo-text-container">
+                    <p>Nivel de SEO</p>
+                    <p><?= $seo_level ?>%</p>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="right-container">
+        <div class="right-up">
+            <div class="socials-container">
+                <div class="socials-polar-parent">
+                    <canvas id="socials-polar"></canvas>
+                </div>
+                <div class="socials-text-container"></div>
+            </div>
+            <div class="bounce-rate-container">
+                <p>Tasa de Rebote</p>
+                <div class="bounce-rate-parent">
+                    <canvas id="bounce-rate"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="right-down">
+            <div class="sales-container">
+                <div class="sales-text-container">
+                    <p>Ventas 1er Trimestre</p>
+                    <p class="percentage-number"><?= $sales?>%</p>
+                </div>
+                <div class="sales-polar-parent">
+                    <canvas id="sales-polar"></canvas>
+                </div>
+                <div class="sales-bar-parent">
+                    <canvas id="sales-bar"></canvas>
+                </div>
+                <p>Ultimos 5 dias</p>
+            </div>
+            <div class="projected-earnings-container">
+                <div class="projected-earnings-box">
+                    <p>Ganancias Proyectadas</p>
+                    <p class="percentage-number">$<?= $projected_earnings ?></p>
+                </div>
+                <div class="projected-earnings-linear-parent">
+                    <canvas id="projected-earnings"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
 
     <script>
         // Se declara variable en JS para leerla en js/main.js
         let branding = <?= $branding ?>;
+        let organicGrowth = <?= $organic_growth ?>;
+        let totalGrowth = <?= $total_growth ?>;
+        let seoLevel = <?= $seo_level ?>;
     </script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="js/main.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 const brandChartCtx = document.getElementById('brand-chart');
-const secondChartCtx = document.getElementById('secondChart');
+const orgGrowthDoughnutCtx = document.getElementById('organic-growth-doughnut-chart');
+const orgGrowthLinearCtx = document.getElementById('organic-growth-linear-chart');
+const worldMapCtx = document.getElementById('world-map-chart');
+const totalGrowthDoughnutCtx = document.getElementById('total-growth-doughnut-chart');
+const totalGrowthLinearCtx = document.getElementById('total-growth-linear-chart');
+const seoCtx = document.getElementById('seo-chart');
+const socialsCtx = document.getElementById('socials-polar');
+const bounceRateCtx = document.getElementById('bounce-rate');
+const salesPolarCtx = document.getElementById('sales-polar');
+const salesBarCtx = document.getElementById('sales-bar');
+const projectedEarningsCtx = document.getElementById('projected-earnings');
 
 new Chart(brandChartCtx, {
     type: 'doughnut',
@@ -16,7 +26,6 @@ new Chart(brandChartCtx, {
                 'rgb(0,0,0)',
                 'rgba(0, 0, 0, 0)',
             ],
-            hoverOffset: 4
         }]
     },
     options: {
@@ -28,21 +37,357 @@ new Chart(brandChartCtx, {
         }
     },
 });
-new Chart(secondChartCtx, {
+
+new Chart(orgGrowthDoughnutCtx, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'branding',
+            'filler',
+        ],
+        datasets: [{
+            data: [organicGrowth, 100-organicGrowth],
+            borderColor: 'rgb(0,0,0)',
+            backgroundColor: [
+                'rgb(0,0,0)',
+                'rgba(0, 0, 0, 0)',
+            ],
+        }]
+    },
+    options: {
+        events: [],
+        plugins: {
+            legend: {
+                display:false
+            }
+        }
+    },
+});
+
+new Chart(orgGrowthLinearCtx, {
+    type: 'line',
+    data: {
+        labels: ['labels', 'labels', 'labels', 'labels', 'labels', 'labels', 'labels'],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [5, 12, 28, 51, 71, 87, 96],
+            fill: {
+                target: 'origin',
+                above: 'rgba(98,167,182,0.4)',
+            },
+            borderColor: 'rgb(0,0,0)',
+            tension: 0.1
+        }]
+    },
+    options: {
+        events: [],
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                display: false
+            },
+            y: {
+                display: false
+            }
+        },
+        plugins: {
+            legend: {
+                display:false,
+            }
+        }
+    },
+});
+
+new Chart(worldMapCtx, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+        ],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 205, 86)'
+            ],
+        }]
+    },
+})
+
+new Chart(totalGrowthDoughnutCtx, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'branding',
+            'filler',
+        ],
+        datasets: [{
+            data: [totalGrowth, 100-totalGrowth],
+            borderColor: 'rgb(0,0,0)',
+            backgroundColor: [
+                'rgb(0,0,0)',
+                'rgba(0, 0, 0, 0)',
+            ],
+        }]
+    },
+    options: {
+        events: [],
+        plugins: {
+            legend: {
+                display:false
+            }
+        }
+    },
+});
+
+new Chart(totalGrowthLinearCtx, {
+    type: 'line',
+    data: {
+        labels: ['labels', 'labels', 'labels', 'labels', 'labels', 'labels', 'labels'],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [2, 9, 17, 36, 58, 69, 81],
+            fill: {
+                target: 'origin',
+                above: 'rgba(98,167,182,0.4)',
+            },
+            borderColor: 'rgb(0,0,0)',
+            tension: 0.1
+        }]
+    },
+    options: {
+        events: [],
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                display: false
+            },
+            y: {
+                display: false
+            }
+        },
+        plugins: {
+            legend: {
+                display:false,
+            }
+        }
+    },
+});
+
+new Chart(seoCtx, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'branding',
+            'filler',
+        ],
+        datasets: [{
+            data: [seoLevel, 100-seoLevel],
+            borderColor: 'rgb(0,0,0)',
+            backgroundColor: [
+                'rgb(0,0,0)',
+                'rgba(0, 0, 0, 0)',
+            ],
+        }]
+    },
+    options: {
+        events: [],
+        plugins: {
+            legend: {
+                display:false
+            }
+        }
+    },
+});
+
+new Chart(socialsCtx, {
+    type: 'polarArea',
+    data: {
+        labels: [
+            'Facebook',
+            'Tiktok',
+            'Youtube',
+            'LinkedIn',
+        ],
+        datasets: [{
+            label: 'Socials %',
+            data: [11, 16, 7, 3],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(122,192,75)',
+                'rgb(255, 205, 86)',
+                'rgb(54, 162, 235)'
+            ]
+        }]
+    },
+    options: {
+        scales: {
+            r: {
+                display: false
+            }
+        },
+        plugins: {
+            legend: {
+                position: 'right'
+            }
+        }
+    }
+});
+
+new Chart(bounceRateCtx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Jan', "Feb", 'March', "April", "May", "June"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Bounce Rate',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(201, 203, 207, 0.2)'
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+                'rgb(153, 102, 255)',
+                'rgb(201, 203, 207)'
+            ],
             borderWidth: 1
         }]
     },
     options: {
+        plugins: {
+            legend: {
+                display: false,
+            }
+        },
         scales: {
             y: {
                 beginAtZero: true
             }
         }
+    },
+})
+
+new Chart(salesPolarCtx, {
+    type: 'polarArea',
+    data: {
+        labels: [
+            'Service 1',
+            'Service 2',
+            'Service 3',
+            'Service 4',
+        ],
+        datasets: [{
+            label: 'Services %',
+            data: [3, 11, 8, 14],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(122,192,75)',
+                'rgb(255, 205, 86)',
+                'rgb(54, 162, 235)'
+            ]
+        }]
+    },
+    options: {
+        scales: {
+            r: {
+                display: false
+            }
+        },
+        plugins: {
+            legend: {
+                position: 'right'
+            }
+        }
     }
+});
+
+new Chart(salesBarCtx, {
+    type: 'bar',
+    data: {
+        labels: ['Mon', "Tue", 'Wed', "Thu", "Friday"],
+        datasets: [{
+            label: 'Sales',
+            data: [17, 9, 26, 4, 31],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                display: false,
+            }
+        },
+        scales: {
+            y: {
+                display: false,
+            },
+            x: {
+                display: false,
+            }
+        }
+    },
+})
+
+new Chart(projectedEarningsCtx, {
+    type: 'line',
+    data: {
+        labels: ['labels', 'labels', 'labels', 'labels', 'labels', 'labels', 'labels'],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [2, 9, 17, 36, 58, 69, 81],
+            fill: {
+                target: 'origin',
+                above: 'rgba(98,167,182,0.4)',
+            },
+            borderColor: 'rgb(0,0,0)',
+            tension: 0.1
+        }]
+    },
+    options: {
+        events: [],
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                display: false
+            },
+            y: {
+                display: false
+            }
+        },
+        plugins: {
+            legend: {
+                display:false,
+            }
+        }
+    },
 });
